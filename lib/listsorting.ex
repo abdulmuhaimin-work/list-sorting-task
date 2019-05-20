67cab2a2de4214b5710868972f2dc5aa0cb28e56
abdul muhaimin <blazk@pop-os.localdomain>
2019-05-20 20:48:29 +0800

    initial commit

---
+++ lib/listsorting.ex
@@ -0,0 +1,18 @@
+defmodule Listsorting do
+  @moduledoc """
+  Documentation for Listsorting.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Listsorting.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/listsorting_test.exs
@@ -0,0 +1,8 @@
+defmodule ListsortingTest do
+  use ExUnit.Case
+  doctest Listsorting
+
+  test "greets the world" do
+    assert Listsorting.hello() == :world
+  end
+end
