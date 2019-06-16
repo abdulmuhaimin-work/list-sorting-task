69dccf0836b2930cb36698b34d3e7a43e7f50f58
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2019-06-16 18:48:48 +0800

    first completion of the listsorting module, with simple test

--- lib/listsorting.ex
+++ lib/listsorting.ex
@@ -17,20 +17,44 @@ defmodule Listsorting do
     :world
   end
 
-  def mapcompare(list) do
-    mapcompare(list, [])
+  def higher_prio?(val_a, val_b, step) do
+    cond do
+      val_a[:value] > val_b[:value] -> {true, val_a[:value], val_b[:value], step}
+      val_a[:value] == val_b[:value] and val_a[:key] == "pre" -> {true, val_a[:value], val_b[:value], step}
+      val_a[:value] == val_b[:value] and val_b[:key] == "post" -> {true, val_a[:value], val_b[:value], step}
+      val_a[:value] == val_b[:value] and val_a[:key] == val_b[:key] -> {:same, val_a[:value], val_b[:value], step}
+      val_a[:value] == val_b[:value] and val_b[:key] == "pre" -> {false, val_b[:value], val_a[:value], step}
+      val_a[:value] == val_b[:value] and val_a[:key] == "post" -> {false, val_b[:value], val_a[:value], step}
+      val_a[:value] < val_b[:value] -> {false, val_b[:value], val_a[:value], step}
+      true -> {:error, val_b[:value], val_a[:value], step}
+    end
   end
 
+
+
+  def mapcompare(list) do
+    [first | rest] = list
+    mapcompare(rest, [first])
+  end
+
+
   def mapcompare([mapa,mapb | tail], acc) do
-    if mapb[:value] > mapa[:value] do
-      mapcompare([mapa | tail], acc ++ [mapb])
-      else mapcompare([mapb | tail], acc ++ [mapa])
+
+    with {true, _higher, _lower, 1} <- higher_prio?(mapa, mapb, 1),   # compare 1st map to 2nd map. true if 1st map higher prioritiy, the number at the end is the indicator to identify at which step when result wasnt true
+         {[first | _rest], 2} <- {([first | _rest] = acc) , 2},       # to identify first map in accumulator list. when accumulator is empty [],will fail and then proceed to catch all result. if successful, proceed next step
+         {true, _high, _low, 3} <- higher_prio?(mapb, first, 3)       # compare
+    do
+      mapcompare([mapa | tail], [mapb | acc])
+    else
+      {false , _ , _ , 1} -> mapcompare([mapb | tail], [mapa | acc])  # 1st and 2nd map switch place when 1st is smaller
+      {:same , _ , _ , 1} -> mapcompare([mapa | tail], [mapb | acc])  # just in case theres exact same prio, the one that appear first is higher prio
+      {false , _ , _ , 3} -> mapcompare([mapa | tail], mapcompare([mapb|acc]))
+      _ -> mapcompare([mapa | tail], [mapb | acc])
     end
   end
 
   def mapcompare([mapa], acc) do
-    acc ++ [mapa]
+    [mapa | acc]
   end
-
 
 end
--- test/listsorting_test.exs
+++ test/listsorting_test.exs
@@ -2,37 +2,54 @@ defmodule ListsortingTest do
   use ExUnit.Case
   doctest Listsorting
 
-  test "greets the world" do
-    assert Listsorting.hello() == :world
-  end
-
-  test "3 maplist sort" do
+  test "3 different value maplist sort" do
     maplist =
     [
       %{id: 1, value: 2, key: "action"},
       %{id: 2, value: 4, key: "pre"},
-      %{id: 3, value: 2, key: "post"}
+      %{id: 3, value: 1, key: "post"}
     ]
 
     assert Listsorting.mapcompare(maplist) ==
     [
       %{id: 2, value: 4, key: "pre"},
       %{id: 1, value: 2, key: "action"},
-      %{id: 3, value: 2, key: "post"}
+      %{id: 3, value: 1, key: "post"}
     ]
   end
+
+  test "5 different value maplist sort" do
+    maplist =
+    [
+      %{id: 4, value: 2, key: "action"},
+      %{id: 3, value: 3, key: "action"},
+      %{id: 1, value: 5, key: "action"},
+      %{id: 2, value: 4, key: "pre"},
+      %{id: 5, value: 1, key: "post"}
+    ]
+
+    assert Listsorting.mapcompare(maplist) ==
+    [
+      %{id: 1, value: 5, key: "action"},
+      %{id: 2, value: 4, key: "pre"},
+      %{id: 3, value: 3, key: "action"},
+      %{id: 4, value: 2, key: "action"},
+      %{id: 5, value: 1, key: "post"}
+    ]
+  end
+
 
   test "10 maplist sort" do
     maplist =
     [
       %{id: 1, value: 2, key: "action"},
-      %{id: 2, value: 4, key: "pre"},
+      %{id: 8, value: 4, key: "action"},
       %{id: 3, value: 6, key: "post"},
       %{id: 4, value: 6, key: "action"},
       %{id: 5, value: 4, key: "action"},
       %{id: 6, value: 7, key: "post"},
       %{id: 7, value: 2, key: "pre"},
-      %{id: 8, value: 4, key: "post"},
+      %{id: 2, value: 4, key: "pre"},
       %{id: 9, value: 5, key: "post"},
       %{id: 10, value: 7, key: "action"}
     ]
@@ -45,8 +62,8 @@ defmodule ListsortingTest do
         %{id: 3, value: 6, key: "post"},
         %{id: 9, value: 5, key: "post"},
         %{id: 2, value: 4, key: "pre"},
+        %{id: 8, value: 4, key: "action"},
         %{id: 5, value: 4, key: "action"},
-        %{id: 8, value: 4, key: "post"},
         %{id: 7, value: 2, key: "pre"},
         %{id: 1, value: 2, key: "action"},
 
